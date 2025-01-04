ec53c033e8a436a70b3cdf6788714c03b82247d5
sphinx <k.komandin@gmail.com>
2025-01-04 08:18:47 +0500

    Реализация структур

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,11 +11,12 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	//Cache // Remove me after realization.
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	keys     map[*ListItem]Key
 }
 
 func NewCache(capacity int) Cache {
@@ -21,5 +24,52 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		keys:     make(map[*ListItem]Key, capacity),
+	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mu.Lock()
+	item, ok := c.items[key]
+
+	if ok {
+		item.Value = value
+		c.queue.MoveToFront(item)
+	} else {
+		item = c.queue.PushFront(value)
+		c.items[key] = item
+		c.keys[item] = key
+
+		length := c.queue.Len()
+		if c.capacity > 0 && length > c.capacity {
+			back := c.queue.Back()
+			backKey := c.keys[back]
+			delete(c.items, backKey)
+			delete(c.keys, back)
+			c.queue.Remove(c.queue.Back())
+		}
+	}
+	c.mu.Unlock()
+	return ok
+}
+
+func (c *lruCache) Clear() {
+	c.mu.Lock()
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.mu.Unlock()
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	item, ok := c.items[key]
+	if ok {
+		c.queue.MoveToFront(item)
+
+		return item.Value, ok
+	} else {
+		return nil, false
 	}
 }
--- hw04_lru_cache/go.mod
+++ hw04_lru_cache/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw04_lru_cache
+module github.com/kavaganga/otus_2024/hw04_lru_cache
 
 go 1.22
 
